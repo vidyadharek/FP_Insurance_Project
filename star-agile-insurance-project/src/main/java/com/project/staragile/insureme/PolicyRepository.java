cb90d6a85e803889857af24b5a0e412af0e97875
Vidyadhare <you@example.com>
2023-05-11 18:49:52 +0530

    first commit

--- star-agile-insurance-project/src/main/java/com/project/staragile/insureme/PolicyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.staragile.insureme;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PolicyRepository extends JpaRepository<Policy,Integer>{
-
-}
